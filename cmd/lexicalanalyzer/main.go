58d9048a26f400f258f2831f3dc20e64b7e26d4c
Coral <101916048+Coralnws@users.noreply.github.com>
2025-08-04 21:45:30 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/lexicalanalyzer/main.go
@@ -0,0 +1,23 @@
+// cmd/lexicalanalyzer/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"lexicalanalyzer/internal/lexicalanalyzer"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := lexicalanalyzer.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
